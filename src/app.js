ff311dbaec7e2211724562ef94cbaf90567ceabe
Howard Jing <howard.jing@gmail.com>
2016-10-22 19:22:25 -0400

    add flow

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+// @flow
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './app.css';
 
 class App extends Component {
-  render() {
+  render(): React.Element<React.DOM.div> {
     return (
       <div className="App">
         <div className="App-header">
